274c1c70b560ce2610ac7c7d2cff95a783e21a49
Esteban Borai <estebanborai@gmail.com>
2022-11-26 19:24:42 -0300

    chore: first commit

---
+++ src/cli.rs
@@ -0,0 +1,14 @@
+use clap::{Parser, Subcommand};
+
+#[derive(Parser)]
+#[command(author, version, about, long_about = None)]
+pub struct Cli {
+    #[command(subcommand)]
+    pub command: Option<Command>,
+}
+
+#[derive(Clone, Subcommand)]
+pub enum Command {
+    /// Print current package version
+    Current,
+}
